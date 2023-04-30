7896f33c3a17ff9bdcb37b59e03d7026012a41e4
Rohit Verma <rohit.verma@nslhub.com>
2023-04-30 21:29:11 +0530

    test

--- src/demo/pipeline-stack.ts
+++ src/demo/pipeline-stack.ts
@@ -1,8 +1,10 @@
+import { Construct } from 'constructs';
 import { ApplicationStage } from './application-stage';
 import { DeploymentRecord } from '../common';
 import { SaasPipeline } from '../constructs';
+import { CDK_VERSION } from '../installer/lib/configuration';
 import { Stack } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 interface WorkloadPipelineProps extends DeploymentRecord {
   readonly toolchainKms?: string;
@@ -17,14 +19,34 @@ export class PipelineStack extends Stack {
 
   constructor(scope: Construct, id: string, props: WorkloadPipelineProps) {
     super(scope, id, { env: { account: props.account, region: props.region } });
+    const buildImage = codebuild.LinuxArmBuildImage.fromCodeBuildImageId(props.toolchainImage);
+    const INSTALL_COMMANDS = ['yarn install --immutable --immutable-cache'];
+    const SYNTH_PARAMS = ` -c deployment_type=${props.type} -c deployment_id=${props.id} -c component_account=${props.account} -c component_region=${props.region}`;
+
     this.pipeline = new SaasPipeline(this, `${props.tenantId}-${props.id}-demo`, {
       pipelineName: props.id,
+      cliVersion: CDK_VERSION,
       primarySynthDirectory: 'cdk.out',
       repositoryName: props.repositoryName,
       crossAccountKeys: true,
       synth: {},
+      selfMutation: true,
+      codeBuildDefaults: {
+        cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER),
+      },
+      synthShellStepPartialProps: {
+        installCommands: INSTALL_COMMANDS,
+        commands: ['yarn cdk synth' + SYNTH_PARAMS + ' -y'],
+      },
+      synthCodeBuildDefaults: {
+        buildEnvironment: {
+          computeType: codebuild.ComputeType.SMALL,
+          buildImage: buildImage,
+        },
+      },
       existingKMSKeyAlias: props.toolchainKms,
       existingArtifactBucket: props.toolchainAssetBucket,
+      dockerEnabledForSynth: true,
     });
     const devStage = new ApplicationStage(this, 'Dev', { env: { account: props.account, region: props.region } });
     this.pipeline.addStage(devStage);
