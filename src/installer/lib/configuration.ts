14e15484fa7eb2fc25f635f780250714b2fc6fc6
Rohit Verma <rohit.verma@nslhub.com>
2023-05-01 11:43:42 +0530

    updated test, simplified pipelines

--- src/installer/lib/configuration.ts
+++ src/installer/lib/configuration.ts
@@ -3,8 +3,6 @@ import { DefaultStackSynthesizer } from 'aws-cdk-lib';
 export const DEPLOYMENT_TABLE_NAME = 'saas-pipelines'; // DynamoDB table name that will be created for deployment management
 export const REPOSITORY_OWNER = 'rverma-nsl'; // CodeCommit repository name that holds the code
 export const REPOSITORY_NAME = 'nsl-saas-accelerator'; // CodeCommit repository name that holds the code
-export const REPOSITORY_SECRET = 'saas-provisoner'; // CodeCommit repository name that holds the code
-
 export const GITHUB_DOMAIN = 'token.actions.githubusercontent.com';
 
 export const CDK_VERSION = '2.76.0'; // Used to set CodePipeline CLI version
@@ -16,4 +14,4 @@ export const TOOLCHAIN_ENV = {
 };
 
 export const ASSET_ECR = `${TOOLCHAIN_ENV.account}.dkr.ecr.${TOOLCHAIN_ENV.region}.amazonaws.com/cdk-${DefaultStackSynthesizer.DEFAULT_QUALIFIER}-container-assets-${TOOLCHAIN_ENV.account}-${TOOLCHAIN_ENV.region}`;
-export const ASSET_PARAMETER = '/toolchain/asset-tag'
+export const ASSET_PARAMETER = '/toolchain/asset-tag';
